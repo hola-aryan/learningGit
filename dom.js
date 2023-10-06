8ab87af4d475dd15f42daf2c77423d08bd5d7a13
hola-aryan <aryanisastudent@gmail.com>
2023-10-06 23:40:21 +0530

    Task 6 DOM Manipulation

--- dom.js
+++ dom.js
@@ -45,16 +45,85 @@
     
 // }
 
-var secondItems = document.querySelector('li:nth-child(2)');
-secondItems.style.color = 'blue' ;
+// var secondItems = document.querySelector('li:nth-child(2)');
+// secondItems.style.color = 'blue' ;
 
-var thirdItems = document.querySelector('li:nth-child(3)');
-thirdItems.style.display = 'none' ;
+// var thirdItems = document.querySelector('li:nth-child(3)');
+// thirdItems.style.display = 'none' ;
 
-var liItems = document.querySelectorAll('li');
-liItems[1].style.color = 'green';
+// var liItems = document.querySelectorAll('li');
+// liItems[1].style.color = 'green';
 
-var oddItems = document.querySelectorAll('li:nth-child(odd)');
-for(var i =0; i<oddItems.length; i++){
-    oddItems[i].style.backgroundColor = 'green';
-}
+// var oddItems = document.querySelectorAll('li:nth-child(odd)');
+// for(var i =0; i<oddItems.length; i++){
+//     oddItems[i].style.backgroundColor = 'green';
+// }
+
+//Traversing the DOM
+var itemList = document.querySelector('#items');
+// ParentNode
+// console.log(itemList.parentNode);
+// console.log(itemList.parentNode.parentNode);
+// itemList.parentNode.style.backgroundColor = '#CCC';
+
+// ParentElement
+// console.log(itemList.parentElement);
+// console.log(itemList.parentElement.parentNode);
+// itemList.parentElement.style.backgroundColor = '#CCC';
+
+// // childNode useless
+// console.log(itemList.childNodes);
+
+// // Chidren
+// console.log(itemList.children);
+// itemList.children[1].style.backgroundColor='skyblue';
+
+// // FirstChid - useless
+// console.log(itemList.firstChild);
+
+// // FirstElementChid 
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.style.backgroundColor='yellow';
+
+// // LastChid - useless
+// console.log(itemList.lastChild);
+
+// // LastElementChid 
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.style.backgroundColor='green';
+
+// NextSibling
+// console.log(itemList.nextSibling);
+
+// // NextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// PreviousSibling
+// console.log(itemList.previousSibling);
+
+// NextElementSibling
+// console.log(itemList.previousElementSibling);
+
+// CreateElement
+// Create a div
+var newDiv = document.createElement('div');
+// Add Class
+newDiv.className = 'hello';
+// Add Id
+newDiv.id = 'idhello';
+// Add Attribute
+newDiv.setAttribute('title','Hello Div');
+
+// Create Text Node
+var newDivText = document.createTextNode('Hello Worlds')
+
+newDiv.appendChild(newDivText);
+
+// Placing the div
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+container.insertBefore(newDiv,h1);
+
+newDiv.style.fontSize ='30px';
