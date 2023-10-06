9792886c73ba9198d00edea5e51b6f6e1b508466
hola-aryan <aryanisastudent@gmail.com>
2023-10-06 22:47:31 +0530

    Task 4

--- dom.js
+++ dom.js
@@ -18,21 +18,29 @@
 
 // Get Element by ClassName
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello Pretty';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello Pretty';
 
-items[2].style.backgroundColor = 'green';
+// items[2].style.backgroundColor = 'green';
 
-for(var i=0; i<items.length; i++){
-    items[i].style.fontWeight = 'bold';
+// for(var i=0; i<items.length; i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+// var item = document.getElementById('main-header');
+// item.innerText='La La La La La';
+// item.style.border ='2px solid black';
+
+// var items = document.getElementById('main-header');
+
+// console.log(document.getElementById('hello').textContent);
+
+var listItems= document.getElementsByTagName('li');
+console.log(listItems);
+for(var i = 0; i<listItems.length; i++){
+    if(i===4)
+        listItems[i].style.backgroundColor = 'skyblue';
+    
 }
-
-var item = document.getElementById('main-header');
-item.innerText='La La La La La';
-item.style.border ='2px solid black';
-
-var items = document.getElementById('main-header');
-
-console.log(document.getElementById('hello').textContent);
